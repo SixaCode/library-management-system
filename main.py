8e4189b57d464dcb85d72f887a03c3a5fdc756e9
Brian Steven Gutierrez Prieto <mrjoer22@gmail.com>
2025-10-23 21:27:14 -0500

    Created main menu

--- main.py
+++ main.py
@@ -38,24 +38,48 @@ class Library:
                 print(book)
                 print("===================================")
 
-# Main program execution
+
+######################
+# FUNCTIONS FOR MENU #
+######################
+
+
+
+##########################
+# MAIN PROGRAM EXECUTION #
+##########################
+
 if __name__ == "__main__":
+    
+    while(True):
 
-    # Main menu
+        # Main menu
 
-    option = -1
-    print("""Welcome to the Library Management System
+        print("""Welcome to the Library Management System
           1. List all books
           2. List all available books
           3. Add a book
           4. Remove a book
-          5. """)
-    
-    while(True):
+          5. Reserve a book
+          0. Exit""")
+        
+        option = int(input("Choose your option: "))
+        
         match option:
-            case 0:
-                
+            case 0:     # Exit program
                 break
+            case 1:     # List all books
+                pass
+            case 2:     # List all available books
+                pass
+            case 3:     # Add a book
+                pass
+            case 4:     # Remove a book
+                pass
+            case 5:     # Reserve a book
+                pass
+            case _:     # Invalid option
+                print("Invalid option. Please try again.")
 
 
 
